ac200fc659471f78cdea7e7ec23a56f706b55255
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-04-05 10:50:57 -0300

    modulo javascript intermediário completo

---
+++ modelo-javascript-intemediario/20-resolvendo-erros-js/index.js
@@ -0,0 +1,20 @@
+
+
+// var valorBoleto = 100;
+
+// console.log(valorBoleto);
+
+
+
+let inputTelefone = document.getElementById("telefone");
+
+console.log(inputTelefone);
+
+
+inputTelefone.disabled = true;
+
+
+
+function habilitarTelefone() {
+  telefone.disabled = false;
+}
