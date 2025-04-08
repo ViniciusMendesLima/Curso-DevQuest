15252c62f430fa4445a31c3569678d6c267c9143
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-04-07 21:52:04 -0300

    exercicio 5 finalizado

--- Exercicios/javascript-intermediario/5-carrossel-imagens/index.js
+++ Exercicios/javascript-intermediario/5-carrossel-imagens/index.js
@@ -1,11 +1,51 @@
-const inputs= document.querySelectorAll('input')
+let sliderIndex = 0
 
-inputs.forEach(input => {
-    input.addEventListener('change', () => {
-        if (input.value.trim() !== '') {
-            input.classList.add('texto')
-        } else {
-            input.classList.remove('texto')
-        }
-    })
+sliderDivs = document.querySelectorAll(".slider")
+maxSliderDivs =  sliderDivs.length-1
+
+let prox = document.getElementById("seta-direita")
+let voltar = document.getElementById("seta-esquerda")
+
+function checarsetaprox(){
+    if (sliderIndex === maxSliderDivs){
+        prox.src = "images/seta direita opacidade.png"
+        prox.style.pointerEvents = "none"
+    }else{
+        prox.src = "images/seta direita.png"
+        prox.style.pointerEvents = "auto"
+    }
+}
+
+function checarsetavoltar(){
+    if (sliderIndex > 0){
+        voltar.src = "images/seta esquerda.png"
+        voltar.style.pointerEvents = "auto"
+    }else{
+        voltar.src = "images/seta esquerda opacidade.png"
+        voltar.style.pointerEvents = "none"
+    }
+}
+
+prox.addEventListener('click', () => {
+    if (sliderIndex < maxSliderDivs) {
+        sliderDivs[sliderIndex].classList.remove("selecionado")
+        sliderIndex++
+        sliderDivs[sliderIndex].classList.add("selecionado")
+    }
+    checarsetaprox();
+    checarsetavoltar();
 })
+
+voltar.addEventListener('click', () => {
+    if (sliderIndex > 0) {
+        sliderDivs[sliderIndex].classList.remove("selecionado")
+        sliderIndex--
+        console.log("maximo "+maxSliderDivs);
+        console.log(sliderIndex);
+        sliderDivs[sliderIndex].classList.add("selecionado")
+    }
+    checarsetaprox();
+    checarsetavoltar();
+})
+
+
