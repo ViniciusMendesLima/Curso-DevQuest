c73e1a8373b60141a5890c4d9eb00575011cdeee
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-04-08 23:06:45 -0300

    resolução carrossel de imagens

--- Exercicios/javascript-intermediario/5-carrossel-imagens-resolução/src/js/painel.js
+++ Exercicios/javascript-intermediario/5-carrossel-imagens-resolução/src/js/painel.js
@@ -10,16 +10,46 @@ setaAvancar.addEventListener("click", function() {
         return;
     }
     
-    esconderImagemAberta();
-
     imagemAtual++;
-
-    imagens[imagemAtual].classList.add("mostrar")
- 
-    console.log(imagemAtual);
+    esconderImagemAberta();
+    mostrarImagem();
+    mostrarouEsconderSetas()
 
 });
+
+setaVoltar.addEventListener("click", function() {
+    
+    if (imagemAtual === 0){
+        return;
+    }
+
+    imagemAtual--;
+    esconderImagemAberta();
+    mostrarImagem();
+    mostrarouEsconderSetas()
+
+});
+function mostrarImagem() {
+    imagens[imagemAtual].classList.add("mostrar");
+}
+
 function esconderImagemAberta(){
     const imagemAberta = document.querySelector(".mostrar");
     imagemAberta.classList.remove("mostrar");
+}
+
+function mostrarouEsconderSetas(){
+    const nãoEhAPrimeiraImagem = imagemAtual !== 0 ;
+    if (nãoEhAPrimeiraImagem) {
+        setaVoltar.classList.remove("opacidade");
+    } else {
+        setaVoltar.classList.add("opacidade")
+    }
+
+    const chegouNaUltimaimagem = imagemAtual !== 0 && imagemAtual === imagens.length - 1;
+    if (chegouNaUltimaimagem) {
+        setaAvancar.classList.add("opacidade");
+    } else {
+        setaAvancar.classList.remove("opacidade");
+    }
 }
