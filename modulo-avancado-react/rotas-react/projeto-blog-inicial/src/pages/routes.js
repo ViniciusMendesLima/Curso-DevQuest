408439d68643e232ee6f5214a8ff13df14955d25
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-06-09 16:17:48 -0300

    aula 14

--- modulo-avancado-react/rotas-react/projeto-blog-inicial/src/pages/routes.js
+++ modulo-avancado-react/rotas-react/projeto-blog-inicial/src/pages/routes.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Posts } from './posts'
-// import { Post } from './post'
+import { Post } from './post'
 
 const AppRoutes = () => (
     <BrowserRouter>
         <Routes>
             <Route exact path='/' element={<Posts />} />
+            <Route exact path='/post/:id' element={<Post />} />
         </Routes>
     </BrowserRouter>
 )
