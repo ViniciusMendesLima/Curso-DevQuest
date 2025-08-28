2e6487c0a76aeed44347fa9e0d342eb4d5995af7
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-08-28 20:39:49 -0300

    todo list em andamento

---
+++ modulo-fundamentos-react/todolist-app/src/App.tsx
@@ -0,0 +1,13 @@
+import TodoHeader from "./components";
+
+function App() {
+  return (
+    <main className="bg-neutral-very-dark-blue h-screen">
+      <div className="bg-[url('/images/bg-desktop-dark.jpg')] h-80 bg-cover bg-center">
+        <TodoHeader/>
+      </div>
+    </main>
+  );
+}
+
+export default App;
